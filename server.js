fcfa19de959a7103e7c5ab6aeaecbc2281091617
HankHill <hhc3h8@gmail.com>
2022-10-07 18:54:25 -0500

    changed secret

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({ helpers });
 
 const sess = {
-    secret: 'Super secret secret',
+    secret: '3000 Black Jets of Allah',
     cookie: {
         maxAge: 60 * 60 * 1000,
         httpOnly: false,
