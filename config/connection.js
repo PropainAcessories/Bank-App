8da1153063a281bbbcebfb2932174d4a1aea2497
HankHill <hhc3h8@gmail.com>
2022-10-08 19:00:25 -0500

    config cleanup

--- config/connection.js
+++ config/connection.js
@@ -1,31 +1,17 @@
-const Sequelize = require('sequelize');
+const Sequelize = require ('sequelize');
 
-require('dotenv').config();
+require ('dotenv').config();
 
-// let sequelize;
+let sequelize;
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+    sequelize = new Sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PW,
+    )
+}
 
-// if(process.env.JAWSDB_URL) {
-//     sequelize = new Sequelize(process.env.JAWSDB_URL);
-// } else {
-//     sequelize = new Sequelize(
-//         process.env.DB_NAME,
-//         process.env.DB_USER,
-//         process.env.DB_PASSWORD,
-//         {
-//             host: 'localhost',
-//             dialect: 'mysql',
-//             port: 3306
-//         }
-//     );
-// }
-const sequelize = process.env.JAWSDB_URL ? new Sequelize(process.env.JAWSDB_URL) : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: 'localhost',
-
-    dialect: 'mysql',
-
-    dialectOptions: {
-        decimalNumbers: true,
-    },
-});
 
 module.exports = sequelize;
