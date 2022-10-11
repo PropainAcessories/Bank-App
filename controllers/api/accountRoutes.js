28bef7b9cbeaed63dce2e93767336b634bac166b
HankHill <hhc3h8@gmail.com>
2022-10-10 19:40:21 -0500

    checking routes

--- controllers/api/accountRoutes.js
+++ controllers/api/accountRoutes.js
@@ -2,18 +2,20 @@ const router = require('express').Router();
 const { Account } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-router.get('/', withAuth, async (req, res) => {
-    try {
-        const accountData = await Account.findAll({
-            attributes: { exclude: ['pin'] },
-            order: [['id', 'ASC'],]
-        });
+// Re-AWAKEN when we are able to verify between employee and customer
 
-        res.status(200).json(accountData);
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
+// router.get('/', withAuth, async (req, res) => {
+//     try {
+//         const accountData = await Account.findAll({
+//             attributes: { exclude: ['pin'] },
+//             order: [['id', 'ASC'],]
+//         });
+
+//         res.status(200).json(accountData);
+//     } catch (err) {
+//         res.status(500).json(err);
+//     }
+// });
 
 router.get('/:id', withAuth, async (req, res) => {
     try {
