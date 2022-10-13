2e33df0f689cce3d5a2f2736ccffd3c15eff0b4b
HankHill <hhc3h8@gmail.com>
2022-10-12 20:49:38 -0500

    commit

--- controllers/api/user-Routes.js
+++ controllers/api/user-Routes.js
@@ -29,6 +29,7 @@ router.get('/:id', async (req, res) => {
         res.json.status(200).json(userData);
     } catch (err) {
         res.status(500).json(err);
+        console.log(err);
     }
 });
 
