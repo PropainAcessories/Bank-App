8c956dcbc1d187afca315a10b7984cd47425604d
HankHill <hhc3h8@gmail.com>
2022-10-17 19:58:57 -0500

    transactions

--- controllers/api/user-Routes.js
+++ controllers/api/user-Routes.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 const { User } = require('../../models');
-const withAuth = require('../../utils/auth');
 const bcrypt = require('bcrypt');
 
 router.get('/', async (req, res) => {
