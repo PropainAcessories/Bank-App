73201630dcc4001e6367c3a441af1961b2a1bb21
HankHill <hhc3h8@gmail.com>
2022-10-19 18:11:46 -0500

    removed lucky lucre

--- public/js/main.js
+++
@@ -1,8 +0,0 @@
-function dropDown(x) {
-    x.classList.toggle("change");
-  
-    document.getElementById("myDropdown").style.display =
-      document.getElementById("myDropdown").style.display == "block"
-        ? "none"
-        : "block";
-  }
