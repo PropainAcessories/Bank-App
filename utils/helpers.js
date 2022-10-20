bed1114db5782589938a25bf050b8bfd8b1ebee6
HankHill <hhc3h8@gmail.com>
2022-10-20 13:50:42 -0500

    dayjs failsafe; transactions not working in deployment

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,5 +1,13 @@
 module.exports = {
     // Helper functions
+    current_date: () => {
+        const dayjs = require('dayjs');
+        let now = dayjs().format('MMMM-DD-YYYY').toString();
+
+        console.log(now);
+
+        return now;
+    },
     isEmployee: employee => {
         const isemployee = false;
         if (!isemployee) {
